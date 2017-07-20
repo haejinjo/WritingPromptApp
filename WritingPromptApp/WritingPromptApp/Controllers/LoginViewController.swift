cc3a16baf29fd85c2f057671c4cff5d3010dd259
Haejin <haejinjo@g.ucla.edu>
2017-07-19 17:04:55 -0700

    no more firebase sigabrt, runs

---
+++ WritingPromptApp/WritingPromptApp/Controllers/LoginViewController.swift
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.swift
+//  WritingPromptApp
+//
+//  Created by Haejin Jo on 7/19/17.
+//  Copyright © 2017 Haejin Jo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+}
