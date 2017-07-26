b7833c5821594aa949a7c28aff638f7630260695
Haejin <haejinjo@g.ucla.edu>
2017-07-26 14:42:22 -0700

    refactor: decouple networking code for creating users in firebase from view controllers into service struct

--- WritingPromptApp/WritingPromptApp/Controllers/CreateUsernameViewController.swift
+++ WritingPromptApp/WritingPromptApp/Controllers/CreateUsernameViewController.swift
@@ -23,23 +23,11 @@ class CreateUsernameViewController: UIViewController {
             let username = usernameTextField.text,
             !username.isEmpty else {return}
         
-        // create a dict to properly store the name provided by user into our database
-        let userAttrs = ["username": username]
-        
-        let ref = Database.database().reference().child("users").child(firUser.uid)
-        
-        
-        // write data we want to store in FBdatabase
-        ref.setValue(userAttrs) { (error, ref) in
-            if let error = error {
-                assertionFailure(error.localizedDescription)
-                return
-            }
-        
-            // read user data we just wrote to FBdatabase and create new User object from it
-            ref.observeSingleEvent(of: .value, with: { (snapshot) in
-                let user = User(snapshot: snapshot)
-            })
+        UserService.create(firUser, username: username) { (user) in
+            guard let user = user else {return}
+            
+            print("Created new user: \(user.username)")
         }
+    
     }
 }
---
+++ WritingPromptApp/WritingPromptApp/Services/UserService.swift
@@ -0,0 +1,43 @@
+//
+//  UserService.swift
+//  WritingPromptApp
+//
+//  Created by Haejin Jo on 7/26/17.
+//  Copyright © 2017 Haejin Jo. All rights reserved.
+//
+
+import Foundation
+import FirebaseDatabase
+
+// Service structs are middle men between our app and Firebase
+
+// GOAL :Remove networking-related code from view controlleres into "service layer"
+
+// to encapsulate functionality for creating a user on FIrebase
+struct UserService {
+    
+    static func create(_ firUser: FIRUser, username: String, completion: @escaping (User?) -> Void) {
+        
+        
+        let ref = Database.database().reference().child("users").child(firUser.uid)
+        
+         // create a dict to properly store the name provided by user into our database
+        let userAttrs = ["username": username]
+        
+                // write data we want to store in FBdatabase
+        ref.setValue(userAttrs) { (error, ref) in
+            if let error = error {
+                assertionFailure(error.localizedDescription)
+                return completion(nil)
+            }
+            
+            // read user data we just wrote to FBdatabase and create new User object from it
+            ref.observeSingleEvent(of: .value, with: { (snapshot) in
+                let user = User(snapshot: snapshot)
+                completion(user)
+                
+            })
+        }
+    }
+
+}
